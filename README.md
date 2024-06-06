3cf25eba6bc44c22f847bd5fb427adc69ac94d03
KartikKARTIK-4498 <kartik.khangwal1@gmail.com>
2024-06-06 12:06:28 +0200

    Update

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-
 # Keepintouch
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.3.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        'infinite-scroll': 'infinite-scroll 25s linear infinite',
+      },
+      keyframes: {
+        'infinite-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        }
+      }    
+    },
   },
   plugins: [
     require('daisyui'),
